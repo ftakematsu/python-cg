7c4b30364d4e950701ca810fe7ecd36a77113bb0
Fabio Matsunaga <ftakematsu@gmail.com>
2023-08-02 21:51:35 -0300

    fix: leitor de arquivo

--- 4-leitor-arquivo/main.py
+++ 4-leitor-arquivo/main.py
@@ -22,18 +22,37 @@ def setViewport(left, right, bottom, top):
 Leitor do arquivo
 '''
 def readFile(fileName):
-    pass
+    modo = 0 # Modo 0: primeiro desenho, Modo 1: não é o primeiro
+    with open(fileName) as f:
+        for line in f:
+            # Separa a string por espaço
+            linhaAux = line.strip().split(' ')
+            #print(linhaAux)
+            if (len(linhaAux)==1): # Se a lista for tamanho 1
+                if (modo==0):
+                    glBegin(GL_LINE_STRIP)
+                    modo=1
+                else:
+                    glEnd() # Importante para indicar o fim de uma sequência de linhas
+                    glBegin(GL_LINE_STRIP) # Inicia a próxima figura
+            elif (len(linhaAux)==2): # Indica ser uma coordenada xy
+                glVertex2f(float(linhaAux[0]), float(linhaAux[1]))
+    glEnd() # Finaliza o desenho de forma geral
+    glFlush() # Sempre faz uma atualização da tela
+
 
 def draw():
     setWindow(0.0, 640.0, 0.0, 480.0)
-    readFile("exemplo.dat")
+    glColor3f(0.0, 0.0, 0.0)
+    readFile("C:\\Users\\senai\\Documents\\python-cg\\4-leitor-arquivo\\dino.dat")
 
 
 def main():
     pygame.init()
-    display = (800, 600)
+    display = (800, 800)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     pygame.display.set_caption('Computação Gráfica')
+    #readFile("C:\\Users\\senai\\Documents\\python-cg\\4-leitor-arquivo\\exemplo.dat")
     init()
     setWindow(-5.0, 5.0, -0.3, 1.0)
     while True:
@@ -41,7 +60,6 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
         glClear(GL_COLOR_BUFFER_BIT)
         draw()
         pygame.display.flip()
