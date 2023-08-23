62e04c939b5cf28b57668dee755102bbfeb8a4c7
Fabio <ftakematsu@gmail.com>
2023-08-23 20:21:06 -0300

    versão múltiplas figuras

--- 5-editor-poligono/main.py
+++ 5-editor-poligono/main.py
@@ -36,9 +36,8 @@ class Ponto:
         return "(" + str(self.x) + ", " + str(self.y) + ")"
 
 class Poligono:
-    pontos = []
     def __init__(self):
-        pass
+        self.pontos = []
     def add(self, ponto: Ponto):
         self.pontos.append(ponto)
     def draw(self):
@@ -49,6 +48,9 @@ class Poligono:
             glVertex2d(ponto.x, ponto.y)
         glEnd()
         glFlush()
+    def printData(self):
+        for ponto in self.pontos:
+            print(str(ponto))
 
 class Tela:
     ponto = 1.0
@@ -57,6 +59,7 @@ class Tela:
     cor = RGB(0.0, 0.0, 0.0)
     poligonos = [] # Lista de objetos Poligono
     polAtual = 0 # Índice do polígono atual
+    qtdPoligonos = 1 # Quantidade total de poligonos
     def __init__(self, px, py, title):
         self.pX = px # Largura da janela
         self.pY = py # Altura da janela
@@ -72,6 +75,17 @@ class Tela:
         glColor3f(self.cor.red, self.cor.green, self.cor.blue)
         self.poligonos.append(Poligono())
 
+    def proximoPoligono(self):
+        self.polAtual += 1
+        if (self.polAtual==self.qtdPoligonos):
+            self.poligonos.append(Poligono())
+            self.qtdPoligonos+=1
+
+    def poligonoAnterior(self):
+        # Para validação, apenas se valor >0
+        if (self.polAtual>0):
+            self.polAtual-=1
+
     def addPonto(self, ponto: Ponto):
         # Conversão da dimensão das coordenadas matriciais
         # para a dimensão do plano Cartesiano
@@ -79,10 +93,26 @@ class Tela:
         self.poligonos[self.polAtual].add(ponto)
 
     def draw(self):
-        self.poligonos[0].draw()
+        # Criar uma repetição while para desenhar cada polígono da lista
+        i=0
+        while (i<self.qtdPoligonos):
+            if (i==self.polAtual):
+                glLineWidth(3.0)
+            else:
+                glLineWidth(1.0)
+            self.poligonos[i].draw()
+            i+=1 # Incremento para evitar looping infinito
+
+    def printData(self):
+        i=0
+        while (i<self.qtdPoligonos):
+            print(f"Poligono {i}")
+            self.poligonos[i].printData()
+            i+=1
+
 
 def main():
-    tela = Tela(500, 500, "Editor de polígonos")
+    tela = Tela(900, 900, "Editor de polígonos")
     while True: # Looping para capturar eventos
         # Iteração sobre uma lista de eventos
         for event in pygame.event.get():
@@ -95,6 +125,19 @@ def main():
                 #print("Voce clicou em: " + str((posX, posY)))
                 # Adicionando a região do pixel x,y como coordenada
                 tela.addPonto(Ponto(posX, posY))
+            # Eventos relacionados a teclado
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    tela.printData()
+                if event.key == pygame.K_RIGHT:
+                    tela.proximoPoligono()
+                    print("Proximo: " )
+                    print(str(tela.polAtual))
+                elif event.key == pygame.K_LEFT:
+                    tela.poligonoAnterior()
+                    print("Anterior: " )
+                    print(str(tela.polAtual))
+
         glClear(GL_COLOR_BUFFER_BIT)
         tela.draw()
         pygame.display.flip()
